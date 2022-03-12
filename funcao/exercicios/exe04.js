005526fc26af094de231e943546ee478f49ff2ae
Richard Brederode <richardbrederode@hotmail.com>
2022-03-12 18:36:58 -0300

    atualização

--- funcao/exercicios/exe04.js
+++ funcao/exercicios/exe04.js
@@ -1,6 +1,3 @@
 /*04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
 e o resto da divisão destes dois valores.  */
 
-vivo 932933694
-
-tim 963430080
