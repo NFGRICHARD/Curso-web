74dc8d5ad1de5b1f515f61ed1c7f94950afba03c
Richard Brederode <richardbrederode@hotmail.com>
2022-04-03 18:57:39 -0300

    exercício 30

---
+++ funcao/exercicios/exe30.js
@@ -0,0 +1,2 @@
+//30 Escreva um algoritmo que percorre um vetor de inteiros e defina o maior e menor valor dentro do vetor
+
--- funcao/exercicios/exo29.js
+++ funcao/exercicios/exo29.js
@@ -1,3 +1,17 @@
 /*29  Utilizando a estrutura de repetição for, faça uma função que percorra um vetor e conte quantos números
-deste vetor estão no intervalo [10,20] (repare que o intervalo é fechado, ou seja, inclui o 10 e o 20) e quantos
-deles estão fora do intervalo, escrevendo estas informações.*/
+deste vetor estão no intervalo [10,20] (repare que o intervalo é fechado, ou seja, inclui o 10 e o 20) e quantos deles estão fora do intervalo, escrevendo estas informações.*/
+
+
+function leitor(vetorNumeros){
+    let conte = 0
+    for(let i = 0; i < vetorNumeros.length; i++){
+        if(vetorNumeros[i] >= 10 && vetorNumeros[i] <= 20){
+            conte++
+        }
+    }
+    return`${conte}`
+}
+
+const vetorNumeros = [7, 8, 9, 10, 11, 12, 16, 17, 18, 19, 20, 2]
+
+console.log(leitor(vetorNumeros))
